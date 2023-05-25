2dacadbde83c0ec15b5379b2bcec1bd8f7f7acb9
kero-sene <johnm.vergis@gmail.com>
2023-05-25 14:11:34 -0700

    completed Goal module

--- test/test_goal.py
+++ test/test_goal.py
@@ -6,26 +6,28 @@ class GoalTests(unittest.TestCase):
     """Subclass of TestCase for the subclass Goal of class Event."""
     def test_init_ok(self):
         """Assert that the class properly initializes with given parameters."""
-        goal = Goal("name", 0.99, 1, datetime.datetime.strptime('05/25/2023','%m/%d/%Y'))
-        self.assertEqual(goal.target_date, datetime.datetime.strptime('05/25/2023','%m/%d/%Y'))
+        goal = Goal("name", 0.99, datetime.datetime.strptime('05/25/2023','%m/%d/%Y').date())
+        self.assertEqual(goal.name, "name") #Should initialize all parameters properly
+        self.assertEqual(goal.weight, 0.99)
+        self.assertEqual(goal.target_date, datetime.datetime.strptime('05/25/2023','%m/%d/%Y').date())
 
     def test_init_bad_name(self):
         """Assert that the class raises an error when given a bad name."""
         with self.assertRaises(TypeError):
-            goal = Goal(123, 0.99, 0, datetime.datetime.strptime('05/25/2023','%m/%d/%Y')) #Name should be string
+            goal = Goal(123, 0.99, datetime.datetime.strptime('05/25/2023','%m/%d/%Y').date()) #Name should be string
             
     def test_init_bad_weight(self):
         """Assert that the class raises an error when given a bad weight."""
         with self.assertRaises(ValueError):
-            goal = Goal("name", -0.01, 0, datetime.datetime.strptime('05/25/2023','%m/%d/%Y')) #Weight should be a float, between or equal to 0 and 1.
+            goal = Goal("name", -0.01, datetime.datetime.strptime('05/25/2023','%m/%d/%Y').date()) #Weight should be a float, between or equal to 0 and 1.
         with self.assertRaises(ValueError):
-            goal = Goal("name", 1.01, 0, datetime.datetime.strptime('05/25/2023','%m/%d/%Y')) #Weight should be a float, between or equal to 0 and 1.   
+            goal = Goal("name", 1.01, datetime.datetime.strptime('05/25/2023','%m/%d/%Y').date()) #Weight should be a float, between or equal to 0 and 1.   
         with self.assertRaises(TypeError):
-            goal = Goal("name", "this shouldn't work", 0, datetime.datetime.strptime('05/25/2023','%m/%d/%Y')) #Weight should be a float, between or equal to 0 and 1.   
+            goal = Goal("name", "this shouldn't work", datetime.datetime.strptime('05/25/2023','%m/%d/%Y').date()) #Weight should be a float, between or equal to 0 and 1.   
 
     def test_init_bad_date(self):
         """Assert that the class raises an error when given a bad date."""
         with self.assertRaises(TypeError):
-            goal = Goal("name", 0.99, 0, '05/25/2023') #Date should be of datetime.datetime.date type
+            goal = Goal("name", 0.99, '05/25/2023') #target_date should be of datetime.datetime.date type
         with self.assertRaises(ValueError):
-            goal = Goal("name", 0.99, -1, datetime.datetime.strptime('05/22/2023','%m/%d/%Y')) #Date should not be in the past.
+            goal = Goal("name", 0.99, datetime.datetime.strptime('05/22/2023','%m/%d/%Y').date()) #Date should not be in the past.
